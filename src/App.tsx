4a59843f14fe0ddbe795d7a7d28b8ff666e24007
mmgamble <mmgamble@udel.edu>
2023-08-19 11:58:23 -0400

    Setup

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+import "./App.css";
+function App(): JSX.Element {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+
+    return <div className="App">Hello</div>;
+}
+
+export default App;
