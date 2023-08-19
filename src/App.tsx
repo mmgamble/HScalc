428e4407618eb66774d7dfda8e7e8e17e7190817
mmgamble <mmgamble@udel.edu>
2023-08-19 12:28:40 -0400

    react

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import "./App.css";
 function App(): JSX.Element {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
 
-    return <div className="App">Hello</div>;
+    return <div className="App">Hello!</div>;
 }
 
 export default App;
